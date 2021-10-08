5aac2d0a62d6db5cd71431a64e6757a6524af9f6
dubograev <a.dubograev@gmail.com>
2021-10-08 15:32:16 +0300

    added a test

---
+++ src/test/java/ru/dubograev/SecondTest.java
@@ -0,0 +1,12 @@
+package ru.dubograev;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SecondTest {
+
+    @Test
+    void secondTest() {
+        Assertions.assertTrue(true);
+    }
+}
